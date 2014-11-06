0675c919ef2df21bc768e71074e8f6c91de58cbe
Matt Gradwohl <mattgr@msn.com>
2014-11-05 18:59:24 -0800

    Initial checkin

---
+++ URLFix/URLFix/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace URLFix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Location = new System.Drawing.Point(12, 12);
+            this.textBoxIn.Multiline = true;
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(272, 726);
+            this.textBoxIn.TabIndex = 0;
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(290, 12);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(272, 726);
+            this.textBoxOut.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 750);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.textBoxIn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ URLFix/URLFix/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace URLFix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (string s in textBoxIn.Lines)
+            {
+                // trim out any crap
+                String str = s;
+                str = str.Trim();
+                str = str.TrimEnd(',', '\\');
+                str = str.ToLower();
+
+                // create a URL
+                str = "http://" + str;
+                Uri u = new Uri(str);
+
+                // trim subdomains off - first detect if this is a .com style or .co.kr style
+                int cdots = 1;
+                if (u.Host.EndsWith(".com") ||
+                    u.Host.EndsWith(".org") ||
+                    u.Host.EndsWith(".net") ||
+                    u.Host.EndsWith(".edu"))
+                {
+                    cdots = 1;
+                }
+                else
+                {
+                    cdots = 2;
+                }
+
+                // split into the right parts and build a new string with only the .com/.co.kr and the domain
+                String[] site = u.Host.Split('.');
+                if (cdots == 1 || site.Length == 2)
+                {
+                    str = site[site.Length - 2] + '.' + site[site.Length - 1];
+                }
+                else
+                {
+                    str = site[site.Length - 3] + '.' + site[site.Length - 2] + '.' + site[site.Length - 1];
+                }
+
+                // add segments to the url if there are any msn.com/news/sports => /news/sports is the segment
+                if (u.Segments.Length > 1)
+                {
+                    foreach (string t in u.Segments)
+                    {
+                        str += t;
+                    }
+                }
+
+                // issue a web request with the IE11 UA string to see if the site exists
+                try
+                {
+                    String wstr = "http://" + str;
+                    HttpWebRequest wrGETURL;
+                    wrGETURL = (HttpWebRequest) WebRequest.Create(wstr);
+                    wrGETURL.UserAgent = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
+                    wrGETURL.GetResponse();
+                }
+                catch (WebException ex)
+                {
+                    str += "\t" + ex.Status;
+                }
+                
+
+                //Uri f = new Uri(str);
+                textBoxOut.Text += str;
+                textBoxOut.Text += "\r\n";
+            }
+        }
+    }
+}
